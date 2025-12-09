54c80bda5d1a3ef67f07584e3dbe9d5c98e52c06
Arturo Yion Jaime <arturoyj@Mac.local>
2025-12-08 22:05:26 -0600

    archivo para manejar horario

---
+++ app/src/main/java/com/pillup/utils/TimeUtils.kt
@@ -0,0 +1,37 @@
+package com.pillup.utils
+
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Locale
+
+object TimeUtils {
+
+    fun calcularProximaToma(primeraToma: String, intervaloHoras: Int): String {
+        val format = SimpleDateFormat("HH:mm", Locale.getDefault())
+
+        return try {
+            val datePrimeraToma = format.parse(primeraToma) ?: return "Pendiente"
+
+            val calToma = Calendar.getInstance()
+            calToma.time = datePrimeraToma
+
+            val calAhora = Calendar.getInstance()
+
+            val calProxima = Calendar.getInstance().apply {
+                set(Calendar.HOUR_OF_DAY, calToma.get(Calendar.HOUR_OF_DAY))
+                set(Calendar.MINUTE, calToma.get(Calendar.MINUTE))
+                set(Calendar.SECOND, 0)
+                set(Calendar.MILLISECOND, 0)
+            }
+
+            while (calProxima.timeInMillis <= calAhora.timeInMillis) {
+                calProxima.add(Calendar.HOUR_OF_DAY, intervaloHoras)
+            }
+
+            format.format(calProxima.time)
+
+        } catch (e: Exception) {
+            "Error fecha"
+        }
+    }
+}
